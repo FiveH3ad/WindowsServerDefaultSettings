2147ecc64e90402a003f131a950e1a224bd6c60e
Nils Frischknecht <nils.frischknecht@swissrds.ch>
2023-10-17 10:14:38 +0200

    version 0.0.2

--- windows_virtual_machine_default_settings.ps1
+++ windows_virtual_machine_default_settings.ps1
@@ -1,9 +1,21 @@
 Configuration WindowsServerDefaultSettings {
-  Import-DscResource -ModuleName "PSDscResources" -Name WindowsFeature
+  Import-DscResource -ModuleName "PSDscResources" -Name WindowsFeature -ModuleVersion '2.12.0.0'
+  Import-DscResource -ModuleName "PSDscResources" -Name Script -ModuleVersion '2.12.0.0'
+  Import-DscResource -ModuleName "ComputerManagementDsc" -Name TimeZone -ModuleVersion '9.0.0'
+  Import-DscResource -ModuleName "ComputerManagementDsc" -Name SystemLocale -ModuleVersion '9.0.0'
   WindowsFeature 'SNMP' {
     Ensure = 'Present'
     Name   = 'SNMP-Service'
     IncludeAllSubFeature = $true
+  }
+  TimeZone 'SetTimeZone' {
+    TimeZone = 'W. Europe Standard Time'
+    IsSingleInstance = 'Yes'
+
+  }
+  SystemLocale 'SetSystemLocale' {
+    SystemLocale = 'de-CH'
+    IsSingleInstance = 'Yes'
   }
 }
 
